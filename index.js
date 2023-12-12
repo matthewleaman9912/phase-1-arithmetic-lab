71b4ff1d6f61ee95dcba1ddd148d9bac2c469562
MatthewLeaman <leaman.30@osu.edu>
2023-12-11 22:56:45 -0500

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 35;
+let num4 = 31;
+let mod = 35 % 31;
+
+let max = Math.max(1, 2, 4, 5, 10, 15, 20);
